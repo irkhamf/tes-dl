58a887830f0131dd9e27a169911a60e3fb9b8007
keesa17 <64045983+keesa17@users.noreply.github.com>
2024-08-26 16:04:09 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '34ecc1c2-6eac-4796-9f55-929dc4380c84';
 
-const พร็อกซีไอพีs = ['194.58.56.111'];
+const พร็อกซีไอพีs = ['104.248.145.216'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
