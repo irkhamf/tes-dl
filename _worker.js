d5b27d3c56ecc0b6fc6926408a688459ff83c54b
keesa17 <64045983+keesa17@users.noreply.github.com>
2024-08-27 15:46:27 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@
 import { connect } from 'cloudflare:sockets';
 
 const listProxy = [
-    { path: '/id1', proxy: '194.58.56.169' },
-    { path: '/id2', proxy: '202.10.42.30' },
+    { path: '/id1', proxy: '194.58.56.111' },
+    { path: '/id2', proxy: '104.248.145.216' },
     //tambahin sendiri
 ];
 let proxyIP;
